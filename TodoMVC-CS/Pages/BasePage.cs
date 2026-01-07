225e929889d0a33b86cd0817399f4afde0c4fe8a
shanecookofficial <shanecookofficial@gmail.com>
2026-01-07 15:54:25 -0600

    Added the first test for the TodoMVC page.
    Added a GitHub Workflow which will run all tests when a PR is created or updated.

---
+++ TodoMVC-CS/Pages/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace TodoMVC_CS.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Driver { get; }
+
+        protected BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        protected abstract string Url { get; }
+
+        public void Navigate()
+        {
+            Driver.Navigate().GoToUrl(Url);
+        }
+    }
+}
---
+++ TodoMVC-CS/Pages/TodoPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace TodoMVC_CS.Pages
+{
+    public class TodoPage : BasePage
+    {
+        #region Constructor
+        public TodoPage(IWebDriver driver) : base(driver) { }
+        #endregion
+
+        #region Constants
+        protected override string Url => "https://todomvc.com/examples/react/dist/";
+        public static string TitleText => "TodoMVC";
+        public static string HeaderText => "todos";
+        #endregion
+
+        #region Locators
+        private static readonly By TodoHeaderBy =
+            By.XPath("//header/h1");
+        #endregion
+
+        #region Elements
+        public IWebElement TodoHeader => Driver.FindElement(TodoHeaderBy);
+        #endregion
+
+        #region Methods
+        
+        #endregion
+    }
+}
